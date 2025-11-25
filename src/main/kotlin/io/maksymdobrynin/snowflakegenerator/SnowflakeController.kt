bdfcfb2d6d641437c7f3e71bf7fbf1ef3b30689e
dglch <dgalych@icloud.com>
2025-11-25 21:46:07 +0200

    chore:Add controller id logging to SnowflakeController

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -1,10 +1,19 @@
 package io.maksymdobrynin.snowflakegenerator
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController("/v1")
 class SnowflakeController(private val generator: Generator) {
+
+	private val generatorId: String = "Generator-${UUID.randomUUID()}"
+	private val log = LoggerFactory.getLogger(SnowflakeController::class.java)
+
 	@GetMapping("/next-id")
-	suspend fun generate(): Long = generator.nextId()
+	suspend fun generate(): Long {
+		log.info("Controller instance: $generatorId")
+		return generator.nextId()
+	}
 }
